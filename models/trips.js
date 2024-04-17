7c6c6383ab5a136a7c68b60f383166d97d786cf6
Sandrine <sandrine.sardella@gmail.com>
2024-04-17 16:05:07 +0200

    typo models trips

--- models/trips.js
+++ models/trips.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const tripSchema = mongoose.Schema({
-    departure: String;
-    arrival: String;
-    date: Date;
-    price: Number;
+    departure: String,
+    arrival: String,
+    date: Date,
+    price: Number,
 })
 
 
