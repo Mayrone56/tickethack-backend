6f701238ce07b3c6ea208f2f171ff6ee8ed3cab6
Sandrine Sardella <sandrine.sardella@gmail.com>
2024-04-21 23:17:52 +0200

    commit vecel

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 require('./models/connection');
 
 var express = require('express');
--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //URI MongoDB
-const connectionString = 'mongodb+srv://mayrone:WdRCMNqVC1VEWzUN@cluster0.3ult3vz.mongodb.net/tickethackDB';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database tickethack connected'))
