28d4db5ce7263e3ab816c3486ffdb753b4877130
Sandrine Sardella <sandrine.sardella@gmail.com>
2024-06-03 01:02:25 +0200

    commit backend variable environnement + connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-//URI MongoDB
 const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
- .then(() => console.log('Database tickethack connected'))
-  .catch(error => console.error(error));
+ .then(() => console.log('Database connected'))
+ .catch (error => console.error(error));
