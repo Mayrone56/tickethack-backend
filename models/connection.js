76aefa287a66d177ba9cd8a93faa85397f0ef1f8
Sandrine <sandrine.sardella@gmail.com>
2024-06-03 16:02:33 +0200

    commit backend models/connection.js

--- models/connection.js
+++ models/connection.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
- .then(() => console.log('Database connected'))
+ .then(() => console.log('Database TICKETHACK connected'))
  .catch (error => console.error(error));
